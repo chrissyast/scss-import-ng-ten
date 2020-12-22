4f3ed48595befc21186203702c68847e3fd1ba2c
chrissyast <cjastles@gmail.com>
2020-12-22 17:00:10 +0000

    min example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import styles from "../styles.scss";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'vue-to-angular-translation-ten';
+export class AppComponent implements OnInit {
+  //title = 'vue-to-angular-translation-ten';
+  buttonColor = styles["colors-primary"]; //"#387ef5"
+
+
+  ngOnInit() {
+      console.log(styles);
+      console.log(styles["colors-primary"]);
+  }
 }
